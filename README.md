95145dea9dd683dc29d27355ddf2cc6ee945033a
tekintian <tekintian@gmail.com>
2018-03-11 22:11:14 +0800

    with Debian 9-slim 精简升级

--- README.md
+++ README.md
@@ -20,3 +20,54 @@ http://github.com/ynphp
 Tekin
 
 http://dev.yunnan.ws
+
+
+
+PHP_VERSION=7.2.3 \
+    IMAGICK_PECL_VERSION=3.4.3 \
+    LIBMEMCACHED_VERSION=1.0.18 \
+    MEMCACHE_PECL_VERSION=3.0.8 \
+    REDIS_PECL_VERSION=3.1.6 \
+    SWOOLE_VERSION=2.1.1 \
+    OAUTH_PECL_VERSION=2.0.2 \
+    SPHINX_PECL_VERSION=1.3.3 \
+    GNUPG_VERSION=1.4.0 \
+
+
+http://cn2.php.net/distributions/php-7.2.3.tar.gz
+http://www.imagemagick.org/download/ImageMagick.tar.gz
+http://pecl.php.net/get/imagick-3.4.3.tgz
+https://launchpad.net/libmemcached/1.0/1.0.18/+download/libmemcached-1.0.18.tar.gz
+https://github.com/php-memcached-dev/php-memcached/archive/php7.tar.gz -O php-memcached-php7.tar.gz
+http://pecl.php.net/get/redis-3.1.6.tgz
+http://pecl.php.net/get/oauth-2.0.2.tgz 
+https://nchc.dl.sourceforge.net/project/re2c/1.0.1/re2c-1.0.1.tar.gz
+http://pecl.php.net/get/swoole-2.1.1.tgz
+http://pecl.php.net/get/gnupg-1.4.0.tgz
+https://nchc.dl.sourceforge.net/project/re2c/1.0.1/re2c-1.0.1.tar.gz
+http://sphinxsearch.com/files/sphinx-2.2.11-release.tar.gz
+
+https://github.com/tekintian/php/raw/master/src/sphinx-php7-339e123.tar.gz
+
+
+RUN set -x \
+		#install build deps
+		&& apt-get update && apt-get install -y gcc g++ make cmake autoconf libssl-dev  libcurl4-openssl-dev libxslt-dev libicu-dev  libxml2-dev libjpeg-dev libpng-dev libfreetype6-dev libsasl2-dev libevent-dev libpcre3-dev libgpgme11-dev pkg-config patch && rm -rf /var/lib/apt/lists/* \
+
+		......
+
+
+		#remove build deps
+		&& apt-get purge -y --auto-remove gcc g++ make cmake autoconf libssl-dev  libcurl4-openssl-dev libxslt-dev libicu-dev  libxml2-dev libjpeg-dev libpng-dev libfreetype6-dev libsasl2-dev libevent-dev  libpcre3-dev libgpgme11-dev pkg-config patch libgpgme11-dev \
+		&& apt-get clean && apt-get autoclean  \
+		&& cd /tmp/  &&  rm -rf /tmp/* 
+
+
+# 查找删除无用的文件
+  # && rm -rf /usr/local/mysql/bin/*-debug /usr/local/mysql/bin/*_embedded \
+        # && find /usr/local/mysql -type f -name "*.a" -delete \
+        # && apt-get update && apt-get install -y binutils && rm -rf /var/lib/apt/lists/* \
+        # && { find /usr/local/mysql -type f -executable -exec strip --strip-all '{}' + || true; } \
+        # && apt-get purge -y --auto-remove binutils \
+        # && sed -i 's/mirrors.aliyun.com/deb.debian.org/g' /etc/apt/sources.list
+
--- install.sh
+++ install.sh
@@ -4,10 +4,10 @@ PHP_INSTALL_DIR=/usr/local/php
 
 phpExtensionDir=$(${PHP_INSTALL_DIR}/bin/php-config --extension-dir)
 
-[ -z "`grep ^'export PATH=' /etc/profile`" ] && echo "export PATH=$PHP_INSTALL_DIR/bin:\$PATH" >> /etc/profile
-[ -n "`grep ^'export PATH=' /etc/profile`" -a -z "`grep $PHP_INSTALL_DIR /etc/profile`" ] && sed -i "s@^export PATH=\(.*\)@export PATH=$PHP_INSTALL_DIR/bin:\1@" /etc/profile
+[ -z "`grep ^'export PATH=' /etc/profile`" ] && echo "export PATH=${PHP_INSTALL_DIR}/bin:\$PATH" >> /etc/profile
+[ -n "`grep ^'export PATH=' /etc/profile`" -a -z "`grep ${PHP_INSTALL_DIR} /etc/profile`" ] && sed -i "s@^export PATH=\(.*\)@export PATH=${PHP_INSTALL_DIR}/bin:\1@" /etc/profile
 
-cat > $PHP_INSTALL_DIR/etc/php-fpm.conf <<EOF
+cat > ${PHP_INSTALL_DIR}/etc/php-fpm.conf <<EOF
 ;;;;;;;;;;;;;;;;;;;;;
 ; FPM Configuration ;
 ;;;;;;;;;;;;;;;;;;;;;
@@ -64,17 +64,21 @@ env[TEMP] = /tmp
 EOF
 
 # optimize php.ini
-sed -i "s@^memory_limit.*@memory_limit = 192M@" $PHP_INSTALL_DIR/etc/php.ini
-sed -i 's@^output_buffering =@output_buffering = On\noutput_buffering =@' $PHP_INSTALL_DIR/etc/php.ini
-sed -i 's@^;cgi.fix_pathinfo.*@cgi.fix_pathinfo=0@' $PHP_INSTALL_DIR/etc/php.ini
-sed -i 's@^short_open_tag = Off@short_open_tag = On@' $PHP_INSTALL_DIR/etc/php.ini
-sed -i 's@^expose_php = On@expose_php = Off@' $PHP_INSTALL_DIR/etc/php.ini
-sed -i 's@^request_order.*@request_order = "CGP"@' $PHP_INSTALL_DIR/etc/php.ini
-sed -i 's@^;date.timezone.*@date.timezone = Asia/Shanghai@' $PHP_INSTALL_DIR/etc/php.ini
-sed -i 's@^post_max_size.*@post_max_size = 100M@' $PHP_INSTALL_DIR/etc/php.ini
-sed -i 's@^upload_max_filesize.*@upload_max_filesize = 50M@' $PHP_INSTALL_DIR/etc/php.ini
-sed -i 's@^max_execution_time.*@max_execution_time = 600@' $php_install_dir/etc/php.ini
-sed -i 's@^;realpath_cache_size.*@realpath_cache_size = 2M@' $php_install_dir/etc/php.ini
-sed -i 's@^disable_functions.*@disable_functions = passthru,exec,system,chroot,chgrp,chown,shell_exec,ini_alter,ini_restore,dl,openlog,syslog,readlink,symlink,popepassthru,stream_socket_server,popen@' $PHP_INSTALL_DIR/etc/php.ini
-sed -i "s@extension_dir = \"ext\"@extension_dir = \"ext\"\nextension_dir = \"${phpExtensionDir}\"@" $PHP_INSTALL_DIR/etc/php.ini
-[ -e /usr/sbin/sendmail ] && sed -i 's@^;sendmail_path.*@sendmail_path = /usr/sbin/sendmail -t -i@' $PHP_INSTALL_DIR/etc/php.ini
+sed -i "s@^memory_limit.*@memory_limit = 192M@" ${PHP_INSTALL_DIR}/etc/php.ini
+sed -i 's@^output_buffering =@output_buffering = On\noutput_buffering =@' ${PHP_INSTALL_DIR}/etc/php.ini
+sed -i 's@^;cgi.fix_pathinfo.*@cgi.fix_pathinfo=0@' ${PHP_INSTALL_DIR}/etc/php.ini
+sed -i 's@^short_open_tag = Off@short_open_tag = On@' ${PHP_INSTALL_DIR}/etc/php.ini
+sed -i 's@^expose_php = On@expose_php = Off@' ${PHP_INSTALL_DIR}/etc/php.ini
+sed -i 's@^request_order.*@request_order = "CGP"@' ${PHP_INSTALL_DIR}/etc/php.ini
+sed -i 's@^;date.timezone.*@date.timezone = Asia/Shanghai@' ${PHP_INSTALL_DIR}/etc/php.ini
+sed -i 's@^post_max_size.*@post_max_size = 100M@' ${PHP_INSTALL_DIR}/etc/php.ini
+sed -i 's@^upload_max_filesize.*@upload_max_filesize = 50M@' ${PHP_INSTALL_DIR}/etc/php.ini
+sed -i 's@^max_execution_time.*@max_execution_time = 600@' ${PHP_INSTALL_DIR}/etc/php.ini
+sed -i 's@^;realpath_cache_size.*@realpath_cache_size = 2M@' ${PHP_INSTALL_DIR}/etc/php.ini
+sed -i 's@^disable_functions.*@disable_functions = passthru,exec,system,chroot,chgrp,chown,shell_exec,ini_alter,ini_restore,dl,openlog,syslog,readlink,symlink,popepassthru,stream_socket_server,popen@' ${PHP_INSTALL_DIR}/etc/php.ini
+sed -i "s@extension_dir = \"ext\"@extension_dir = \"ext\"\nextension_dir = \"${phpExtensionDir}\"@" ${PHP_INSTALL_DIR}/etc/php.ini
+
+#sendmail config
+ if [ -e /usr/sbin/sendmail ]; then
+  	sed -i 's@^;sendmail_path.*@sendmail_path = /usr/sbin/sendmail -t -i@' ${PHP_INSTALL_DIR}/etc/php.ini
+  fi
